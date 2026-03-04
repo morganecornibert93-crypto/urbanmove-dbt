6200c67d75fd0724505327c9b8a6ff57b3192fbf
morganecornibert93-crypto <morganecornibert93@gmail.com>
2026-03-04 10:46:59 +0100

    Add SQL query to select accessibility data

--- models/example/stg_accessibilite_gare.sql
+++ models/example/stg_accessibilite_gare.sql
@@ -1,1 +1,5 @@
-
+SELECT
+    CAST(stop_point_id AS INT64) AS stop_point_id,
+    CAST(accessibility_level_id AS INT64) AS accessibility_level_id,
+    accessibility_level_name
+FROM `urbanmove-489008.UrbanMove_raw.accessibilite_gare`
