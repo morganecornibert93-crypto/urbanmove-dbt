bfd73c945d6d19368b45a5d13483ab5ae0a28d81
morganecornibert93-crypto <morganecornibert93@gmail.com>
2026-03-04 10:31:52 +0100

    Delete models/staging/stg_accessibilite_gare.sql

--- models/example/models/staging/stg_accessibilite_gare.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    SAFE_CAST(stop_point_id AS INT64) AS stop_point_id,
-    SAFE_CAST(accessibility_level_id AS INT64) AS accessibility_level_id,
-    accessibility_level_name
-FROM `urbanmove-489008.urbanmove_raw.accessibilite_gare`
