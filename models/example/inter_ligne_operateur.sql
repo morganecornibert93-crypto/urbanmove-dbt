2e422219df34827e6fcd3817446a971c0629a030
morganecornibert93-crypto <morganecornibert93@gmail.com>
2026-03-05 12:48:30 +0100

    Mise à jour finale

--- models/example/inter_ligne_operateur.sql
+++ models/example/inter_ligne_operateur.sql
@@ -1,15 +1,4 @@
 SELECT
-    lo.id_ligne,
-    l.nom_ligne,
-    l.mode_transport,
-    o.operatorref AS id_operateur,
-    o.operatorname AS nom_operateur,
-    o.logo
-
-FROM {{ ref('stg_ligne_operateur') }} lo
-
-LEFT JOIN {{ ref('stg_lignes') }} l
-ON lo.id_ligne = l.id_ligne
-
-LEFT JOIN {{ ref('stg_liste_transporteurs') }} o
-ON lo.id_operateur = o.operatorref
+    id_ligne,
+    id_operateur
+FROM {{ ref('stg_referentiel_lignes_clean') }}
