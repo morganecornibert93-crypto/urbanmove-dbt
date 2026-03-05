6429c3f63b13258fff44469bb6f07bb1766eeb2b
morganecornibert93-crypto <morganecornibert93@gmail.com>
2026-03-05 12:49:13 +0100

    Mise à jour finale
    Simplified SQL query by removing unnecessary fields and joins.

--- models/example/inter_ligne_arret.sql
+++ models/example/inter_ligne_arret.sql
@@ -1,17 +1,4 @@
 SELECT
-    la.id_ligne,
-    l.nom_ligne,
-    l.mode_transport,
-    la.id_arret,
-    a.nom_arret,
-    a.latitude,
-    a.longitude,
-    a.niveau_accessibilite
-
-FROM {{ ref('stg_ligne_arret') }} la
-
-LEFT JOIN {{ ref('stg_lignes') }} l
-ON la.id_ligne = l.id_ligne
-
-LEFT JOIN {{ ref('stg_arrets') }} a
-ON la.id_arret = a.id_arret
+    id_ligne,
+    id_arret
+FROM {{ ref('stg_arrets_lignes_clean') }}
