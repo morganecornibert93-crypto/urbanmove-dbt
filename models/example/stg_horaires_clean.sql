05a88fdfdbdb665c554fe9e6a9294d7fa5b03c1b
morganecornibert93-crypto <morganecornibert93@gmail.com>
2026-03-05 12:52:53 +0100

    Mise à jour finale

---
+++ models/example/stg_horaires_clean.sql
@@ -0,0 +1,23 @@
+SELECT
+    date AS date_jour,
+    annee,
+    mois,
+    jour,
+    TRIM(jour_semaine) AS jour_semaine,
+    heure,
+    TRIM(tranche_horaire) AS tranche_horaire,
+    TRIM(SPLIT(ligne_id, ':')[SAFE_OFFSET(1)]) AS id_ligne,
+    TRIM(ligne_nom) as nom_ligne,
+    TRIM(type_transport) AS type_transport,
+    frequence_theorique_par_heure,
+    frequence_reelle_par_heure,
+    taux_service_pct AS taux_service,
+    retard_moyen_minutes,
+    incident_detecte,
+    incident_type,
+    temperature,
+    precipitation,
+    neige,
+    vent,
+    meteo_defavorable 
+FROM {{ source('UrbanMove_raw', 'horaires') }}
