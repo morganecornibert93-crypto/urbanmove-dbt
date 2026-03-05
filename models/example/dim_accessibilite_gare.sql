a066f6cd2f4a59bd39f1bc646980e38ed780fb08
morganecornibert93-crypto <morganecornibert93@gmail.com>
2026-03-05 12:26:29 +0100

    Mise à jour finale

--- models/example/dim_accessibilite_gare.sql
+++ models/example/dim_accessibilite_gare.sql
@@ -1,5 +1,5 @@
 SELECT
     id_niveau_accessibilite,
     nom_niveau_accessibilite
-FROM {{ ref('stg_accessibilite_gare')}}
+FROM {{ ref('stg_accessibilite_gare_clean')}}
 GROUP BY id_niveau_accessibilite, nom_niveau_accessibilite
